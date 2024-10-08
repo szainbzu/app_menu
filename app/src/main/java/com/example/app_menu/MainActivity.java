6675513c5617cac8d9183a426682d389f9de9abd
szainbzu <szain@birzeit.edu>
2024-10-08 10:09:17 +0200

    This is version1

--- app/src/main/java/com/example/app_menu/MainActivity.java
+++ app/src/main/java/com/example/app_menu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.app_menu;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txt = findViewById(R.id.txt);
+        //This is new test code
     }
 }
